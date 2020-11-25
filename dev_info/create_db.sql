244266ad2b8f891202a93b63cff18107e355262b
Myriam Luce <luce.myriam@gmail.com>
2020-11-25 11:06:34 -0600

    Spring cleaning of the folders and files

--- dev_info/create_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE timakan
-WITH OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'fr_CA.utf8'
-    LC_CTYPE = 'fr_CA.utf8'
-    TABLESPACE = pg_default
-    TEMPLATE template0
-    CONNECTION LIMIT = -1;
-alter database timakan set time zone 'America/Montreal';
